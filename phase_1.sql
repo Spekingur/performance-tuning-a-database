edd74e56c4b437d38bf359ff39c55468623cf99d
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2018-04-22 23:04:45 +0000

    more indexes

--- phase_1.sql
+++ phase_1.sql
@@ -3,17 +3,16 @@ USE innheimta;
 dbcc freeproccache; -- Throw away execution plans, among other things
 dbcc dropcleanbuffers; -- Empty the (block) buffer cache
 
---ALTER TABLE dbo.samningur DROP CONSTRAINT samningur_pk;
-
---ALTER TABLE dbo.samningur ADD CONSTRAINT samningur_pk PRIMARY KEY CLUSTERED (id ASC);
-
-CREATE INDEX ix_krafa_samn ON krafa (samningur_id, astand_id);
+CREATE INDEX ix_krafa_samningur ON krafa (samningur_id);
+CREATE INDEX ix_krafa_astand_id ON krafa (astand_id);
+CREATE INDEX ix_samningur_vidskiptamadur_id ON samningur (vidskiptamadur_id);
 CREATE INDEX ix_hreyfing_krafa ON hreyfing (krafa_id);
 CREATE INDEX ix_hreyfing_bokun ON hreyfing (bokunardagur);
 CREATE INDEX ix_hreyfing_tegund_h ON hreyfing (tegund_hreyfingar_id);
 CREATE INDEX ix_hreyfing_innbdagur ON hreyfing (innborgunardagur);
 CREATE INDEX ix_thjodskra_postnumer ON thjodskra (postnumer_id);
 CREATE INDEX ix_heimilisfang_postnumer ON heimilisfang (postnumer_id);
+CREATE INDEX ix_krafa_gjalddagi ON krafa (gjalddagi);
 
 ALTER TABLE vidskiptamadur DROP CONSTRAINT vidskiptamadur_thjodskra_fk;
 ALTER TABLE thjodskra DROP CONSTRAINT thjodskra_pk;
@@ -21,5 +20,6 @@ ALTER TABLE thjodskra ADD CONSTRAINT thjodskra_pk PRIMARY KEY CLUSTERED (kennita
 ALTER TABLE vidskiptamadur ADD CONSTRAINT vidskiptamadur_thjodskra_fk FOREIGN KEY (kennitala)
 	REFERENCES thjodskra (kennitala);
 
---DROP INDEX ix_krafa_samn ON krafa;
 --DROP INDEX ix_hreyfing_krafa ON hreyfing;
+--DROP INDEX ix_krafa_samningur ON krafa;
+--DROP INDEX ix_krafa_astand_id ON krafa;
