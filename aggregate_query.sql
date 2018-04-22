0c0c4a525475811372e19522a6e270b719992702
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2018-04-22 15:14:15 +0000

    some changes, has had no effect on query time

--- aggregate_query.sql
+++ aggregate_query.sql
@@ -8,13 +8,11 @@ SET STATISTICS TIME, IO ON;
 
 -- Summa greiddra krafna einstaklinga, per útibú, eftir póstnúmeri kröfuhafa (skráning í heimilisfang hefur forgang umfram þjóðskrá) og greiðslutímabili. Fyrir svæði utan 1xx póstnúmeraseríunnar.
 SELECT
- distinct k.banki, isnull( ph.postnumer, pt.postnumer ) as Póstnúmer, YEAR( h.bokunardagur ) as Ár, sum( CONVERT( NUMERIC(30,0), k.upphaed_til_greidslu ) ) as Heildarupphæð 
+ distinct k.banki, isnull( ph.postnumer, pt.postnumer ) as Póstnúmer, YEAR( h.bokunardagur ) as Ár, sum( k.upphaed_til_greidslu ) as Heildarupphæð 
 FROM
 	krafa k,
 	samningur s,
-	tegund_logadila tl,
 	hreyfing h,
-	tegund_hreyfingar th,
 	thjodskra t
 	LEFT OUTER JOIN postnumer pt ON ( t.postnumer_id = pt.postnumer ),
 	vidskiptamadur v
@@ -22,20 +20,19 @@ FROM
 	LEFT OUTER JOIN postnumer ph ON ( hf.postnumer_id = ph.postnumer )
 WHERE
 	s.id = k.samningur_id
+	AND k.astand_id = 2
 	AND v.id = s.vidskiptamadur_id
 	AND v.kennitala = t.kennitala
-	AND t.tegund_logadila_id = tl.id
-	AND tl.lysing = 'Einstaklingur'
+	AND t.tegund_logadila_id = 1
 	AND pt.postnumer not like '1%' 
 	AND ph.postnumer not like '1%' 
-	AND k.astand_id in ( SELECT DISTINCT id FROM krafa_astand WHERE lysing = 'Greidd' )
 	AND h.krafa_id = k.id
-	AND h.tegund_hreyfingar_id = th.id
-	AND th.lysing = 'Greiðsla'
+	AND h.tegund_hreyfingar_id = 5
 	AND k.annar_kostnadur is null
 	AND k.annar_vanskilakostnadur is null
 	AND k.fyrri_afslattur is null
 	AND k.seinni_afslattur is null
+	--AND YEAR( h.bokunardagur ) > 2007
 GROUP BY
 	k.banki, isnull( ph.postnumer, pt.postnumer ), YEAR( h.bokunardagur )
 HAVING 
